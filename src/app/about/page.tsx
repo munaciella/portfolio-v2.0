aff431ca8a09cb135110c71e989ee5839944b621
munaciella <franes88@gmail.com>
2024-10-15 17:31:33 +0100

    add a type to check for html span element

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,24 +9,26 @@ import React, { useEffect, useRef } from 'react';
 import profilePic from '../../../public/images/profile/developer-pic-2.jpg';
 import { useInView, useMotionValue, useSpring } from 'framer-motion';
 
-const AnimatedNumbers = ({ value }: { value: number }) => {
-  const ref = useRef(null);
+const AnimatedNumbers = ({ value }: { value: number } | null) => {
+  const ref = useRef<HTMLSpanElement | null>(null); // Explicitly type the ref
   const motionValue = useMotionValue(0);
   const springValue = useSpring(motionValue, { duration: 3000 });
   const isInView = useInView(ref, { once: true });
 
   useEffect(() => {
-    if (isInView) {
+    if (isInView && value !== null) {
       motionValue.set(value);
     }
   }, [isInView, value, motionValue]);
 
   useEffect(() => {
-    springValue.on('change', (latest) => {
-      if (ref.current && Number(latest.toFixed(0)) <= value) {
-        ref.current.textContent = latest.toFixed(0);
-      }
-    });
+    if (value !== null) {
+      springValue.on('change', (latest) => {
+        if (ref.current && Number(latest.toFixed(0)) <= value) {
+          ref.current.textContent = latest.toFixed(0);
+        }
+      });
+    }
   }, [springValue, value]);
 
   return <span ref={ref}></span>;
