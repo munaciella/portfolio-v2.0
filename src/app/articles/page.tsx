5464d9d0b92bbc8cf468438e15be6982a3818896
munaciella <franes88@gmail.com>
2024-10-26 21:37:14 +0100

    fix a ts error on an li element

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -82,9 +82,9 @@ const Article = ({
 }) => {
   return (
     <li
-      initial={{ y: 200 }}
-      whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
-      viewport={{ once: true }}
+    //   initial={{ y: 200 }}
+    //   whileInView={{ y: 0, transition: { duration: 0.5, ease: 'easeInOut' } }}
+    //   viewport={{ once: true }}
       className="relative w-full p-4 py-6 my-14 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4"
     >
       <MovingImg title={title} img={img} link={link} />
