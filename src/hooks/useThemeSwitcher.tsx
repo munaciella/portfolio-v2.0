82b273b6aff16ad359e7cdc4b08c63124c3218fa
munaciella <franes88@gmail.com>
2024-10-28 21:49:57 +0000

    fix hook with a typeof window declaration

--- src/hooks/useThemeSwitcher.tsx
+++ src/hooks/useThemeSwitcher.tsx
@@ -52,15 +52,61 @@
 
 // export default useThemeSwitcher;
 
+
+
+// import React, { useEffect, useState, Dispatch, SetStateAction } from 'react';
+
+// const useThemeSwitcher = (): [string, Dispatch<SetStateAction<string>>] => {
+//   const preferDarkQuery = '(prefers-color-scheme: dark)';
+//   const [mode, setMode] = useState<string>(() => {
+//     return window.localStorage.getItem('theme') || 'light';
+//   });
+
+//   useEffect(() => {
+//     const mediaQuery = window.matchMedia(preferDarkQuery);
+//     const handleChange = (e: MediaQueryListEvent) => {
+//       const newMode = e.matches ? 'dark' : 'light';
+//       setMode(newMode);
+//     };
+
+//     if (!window.localStorage.getItem('theme')) {
+//       const currentTheme = mediaQuery.matches ? 'dark' : 'light';
+//       setMode(currentTheme);
+//     } else {
+//       document.documentElement.classList.toggle('dark', mode === 'dark');
+//     }
+
+//     mediaQuery.addEventListener('change', handleChange);
+//     return () => mediaQuery.removeEventListener('change', handleChange);
+//   }, [mode]);
+
+//   useEffect(() => {
+//     window.localStorage.setItem('theme', mode);
+//     document.documentElement.classList.toggle('dark', mode === 'dark');
+//   }, [mode]);
+
+//   return [mode, setMode];
+// };
+
+// export default useThemeSwitcher;
+
+
+
 import React, { useEffect, useState, Dispatch, SetStateAction } from 'react';
 
 const useThemeSwitcher = (): [string, Dispatch<SetStateAction<string>>] => {
   const preferDarkQuery = '(prefers-color-scheme: dark)';
+
   const [mode, setMode] = useState<string>(() => {
-    return window.localStorage.getItem('theme') || 'light';
+    if (typeof window !== 'undefined') {
+      return window.localStorage.getItem('theme') || 'light';
+    }
+    return 'light';
   });
 
   useEffect(() => {
+    if (typeof window === 'undefined') return;
+
     const mediaQuery = window.matchMedia(preferDarkQuery);
     const handleChange = (e: MediaQueryListEvent) => {
       const newMode = e.matches ? 'dark' : 'light';
@@ -79,8 +125,10 @@ const useThemeSwitcher = (): [string, Dispatch<SetStateAction<string>>] => {
   }, [mode]);
 
   useEffect(() => {
-    window.localStorage.setItem('theme', mode);
-    document.documentElement.classList.toggle('dark', mode === 'dark');
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem('theme', mode);
+      document.documentElement.classList.toggle('dark', mode === 'dark');
+    }
   }, [mode]);
 
   return [mode, setMode];
