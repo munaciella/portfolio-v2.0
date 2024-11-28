490f92454401489fe6a398ad38f85250ff806025
munaciella <franes88@gmail.com>
2024-11-28 21:27:20 +0000

    revert to previous commit

--- src/app/metadata.ts
+++
@@ -1,7 +0,0 @@
-// export const metadata = {
-//     title: 'Francesco_Dev',
-//     description: 'A portfolio website for Francesco',
-//     icons: {
-//       icon: '/favicon.ico',
-//     },
-//   };
