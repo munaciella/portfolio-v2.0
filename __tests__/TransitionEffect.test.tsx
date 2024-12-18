ca225c176c547284b3e4555565debec0aaa7118a
munaciella <franes88@gmail.com>
2024-12-18 14:52:21 +0000

    add test suite for transition effect comp

---
+++ __tests__/TransitionEffect.test.tsx
@@ -0,0 +1,25 @@
+import TransitionEffect from '@/components/TransitionEffect';
+import { render, screen } from '@testing-library/react';
+
+jest.mock('framer-motion', () => ({
+  ...jest.requireActual('framer-motion'),
+  motion: {
+    div: ({ children, ...props }: any) => {
+      console.log(props);
+      return <div {...props} data-testid={`motion-div-${props['data-testid']}`}>{children}</div>;
+    },
+  },
+}));
+
+describe('TransitionEffect Component', () => {
+  it('renders three motion divs with correct classes', () => {
+    render(<TransitionEffect />);
+    
+    const divs = screen.getAllByRole('presentation');
+    expect(divs.length).toBe(3);
+
+    expect(divs[0]).toHaveClass('bg-primary');
+    expect(divs[1]).toHaveClass('bg-light');
+    expect(divs[2]).toHaveClass('bg-dark');
+  });
+});
--- src/components/TransitionEffect.tsx
+++ src/components/TransitionEffect.tsx
@@ -5,23 +5,29 @@ const TransitionEffect = () => {
   return (
     <>
       <motion.div
+        data-testid="motion-div-1"
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-primary"
         initial={{ x: '100%', width: '100%' }}
         animate={{ x: '0%', width: '0%' }}
         exit={{ x: ['0%', '100%'], width: ['0%', '100%'] }}
         transition={{ duration: 0.7, ease: 'easeInOut' }}
+        role="presentation"
       />
       <motion.div
+        data-testid="motion-div-2"
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-light"
         initial={{ x: '100%', width: '100%' }}
         animate={{ x: '0%', width: '0%' }}
         transition={{ delay: 0.2, duration: 0.7, ease: 'easeInOut' }}
+        role="presentation"
       />
       <motion.div
+        data-testid="motion-div-3"
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-dark"
         initial={{ x: '100%', width: '100%' }}
         animate={{ x: '0%', width: '0%' }}
         transition={{ delay: 0.4, duration: 0.7, ease: 'easeInOut' }}
+        role="presentation"
       />
     </>
   );
