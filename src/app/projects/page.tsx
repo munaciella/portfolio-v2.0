544ae29a82f568b359b9485f51f6c248fa8cdaa0
munaciella <franes88@gmail.com>
2025-05-22 15:37:31 +0100

    add latest projects to page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -19,6 +19,13 @@ import project5 from '../../../public/images/projects/Velovegans.jpg';
 import project6 from '../../../public/images/projects/pairsniper.png';
 import project7 from '../../../public/images/projects/Clarity Notion-clone.png';
 import project8 from '../../../public/images/projects/linkedin.png';
+import project9 from '../../../public/images/projects/paperly.png';
+import project10 from '../../../public/images/projects/applywise.png';
+import project11 from '../../../public/images/projects/FileDrop.png';
+import project12 from '../../../public/images/projects/streambuster.png';
+import project13 from '../../../public/images/projects/flowly.png';
+import project14 from '../../../public/images/projects/meteo.png';
+import project15 from '../../../public/images/projects/quillnote.png';
 
 const FramerImage = motion.create(Image);
 
@@ -130,6 +137,62 @@ const ProjectPage: NextPage = () => {
           </p>
 
           <div className="grid grid-cols-3 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 sm:grid-cols-1 gap-12 mt-16">
+            <FeaturedProject
+              title="Paperly"
+              img={project9}
+              summary="A fully functional SaaS application that allows users to upload documents and interact with them using AI-powered chat. It supports various document types, provides chat memory, manages user subscriptions, and includes robust file management capabilities."
+              tag="#NextJS #Firebase #OpenAI #Typescript"
+              link="https://paperly.dev/"
+              github="https://github.com/munaciella/aichat-to-doc"
+            />
+            <FeaturedProject
+              title="Applywise"
+              img={project10}
+              summary="An AI-powered job application toolkit that helps you generate tailored resumes and cover letters using OpenAI, manage saved jobs, and track application statuses, all securely stored in Supabase."
+              tag="#NextJS #Supabase #Typescript #TailwindCSS"
+              link="https://applywise-one.vercel.app/"
+              github="https://github.com/munaciella/resume-ai"
+            />
+            <FeaturedProject
+              title="Filedrop"
+              img={project11}
+              summary="A notes capturing and collaboration platform, enabling seamless note-taking, real-time collaboration, and powerful backend functionalities."
+              tag="#NextJS #Firebase #Clerk #Shadcn-UI"
+              link="https://filedrop-nu.vercel.app/"
+              github="https://github.com/munaciella/filedrop"
+            />
+            <FeaturedProject
+              title="Streambuster"
+              img={project12}
+              summary="A web application that allows users to search for movies and TV shows, view trailers, access detailed information about each title. It also allows the user to save them to their list, track watch progress, and view recommendations. It uses the TMDB API for live data retrieval."
+              tag="#NextJS #Firebase #Typescript #TMDB-API"
+              link="https://streambuster.vercel.app/"
+              github="https://github.com/munaciella/videostore"
+            />
+            <FeaturedProject
+              title="Flowly"
+              img={project13}
+              summary="A real-time, AI-powered Kanban-style task manager that helps teams and individuals organize, prioritise, and collaborate on tasks seamlessly. Built with Firebase Firestore for real-time data, and OpenAI for smart suggestions."
+              tag="#NextJS #Firebase #OpenAI #TailwindCSS"
+              link="https://flowly-chi.vercel.app/"
+              github="https://github.com/munaciella/ai-task-manager"
+            />
+            <FeaturedProject
+              title="The Meteo"
+              img={project14}
+              summary="A responsive, authenticated weather dashboard with Clerk. It fetches data from the free Open‑Meteo API and allows users to save multiple cities to their dashboard with localStorage persistence."
+              tag="#NextJS #Typescript #Open-Meteo-API #TailwindCSS"
+              link="https://the-meteo.vercel.app/"
+              github="https://github.com/munaciella/ultimate-weather"
+            />
+            <FeaturedProject
+              title="QuillNote"
+              img={project15}
+              summary="A Markdown note-taking web app that allows users to create, edit, and delete notes. It features a rich text editor for formatting, a responsive design, and uses Neon DB for data persistence. The app has a Node.js and Express backend with PostgreSQL and OpenAI powered summarisation."
+              tag="#NextJS #Neon #Typescript #Shadcn-UI"
+              link="https://quillnote.vercel.app/"
+              github="https://github.com/munaciella/notes-frontend"
+            />
           <FeaturedProject
               title="Clarity Notes Capturer"
               img={project7}
